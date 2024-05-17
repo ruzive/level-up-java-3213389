b37a8288f565257c952f88617844542cf2c773bb
ruzive <ruzive@gmail.com>
2024-05-17 15:35:43 +0000

    waterbill

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,8 +5,19 @@ import java.util.Scanner;
 public class App {
 
     public static double calculateWaterBill(double gallonsUsage) {
+        double minTwoCCF = 1496.00;
+        double minTwoAmt = 18.84;
+        double oneCCF = 748.00;
+        double oneAmt = 3.90;
+        double usagebill = minTwoAmt;
+        // means we get charged the minimum amount
+        if (minTwoCCF < gallonsUsage) {
+            double c = gallonsUsage - minTwoCCF;
+            double b = Math.ceil(c / oneCCF);
+            usagebill = minTwoAmt + (oneAmt * b);
+        }
         // TODO: Implement method
-        return 0;
+        return usagebill;
     }
 
     public static void main(String[] args) {
